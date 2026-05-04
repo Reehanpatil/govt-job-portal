44e3e7581657f38328ae443ceac32db3be4005d0
Reehan MP <96659823+Reehanpatil@users.noreply.github.com>
2026-05-04 11:43:18 +0530

    Update jobsData.js

--- src/data/jobsData.js
+++ src/data/jobsData.js
@@ -2,6 +2,7 @@ export const jobs = [
   {
     id: 101,
     title: "UPSC Civil Services",
+    slug: "upsc-civil-services",
     category: "latest-jobs",
     location: "All India",
 
@@ -27,6 +28,7 @@ export const jobs = [
   {
     id: 102,
     title: "SSC CGL 2025",
+    slug: "ssc-cgl-2025",
     category: "latest-jobs",
     location: "India",
 
@@ -47,10 +49,110 @@ export const jobs = [
       admitCard: "https://ssc.nic.in/admitcard",
     },
   },
+  {
+    id: 103,
+    title: "UPSC Civil Services",
+    slug: "upsc-civil-services",
+    category: "latest-jobs",
+    location: "All India",
 
+    importantDates: {
+      start: "01 Dec 2025",
+      last: "31 Dec 2025",
+      exam: "April 2026",
+    },
+
+    ageLimit: { min: "21", max: "32" },
+    salary: "₹56,100 – ₹2,50,000",
+    eligibility: "Graduate",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 104,
+    title: "UPSC Civil Services",
+    slug: "upsc-civil-services",
+    category: "latest-jobs",
+    location: "All India",
+
+    importantDates: {
+      start: "01 Dec 2025",
+      last: "31 Dec 2025",
+      exam: "April 2026",
+    },
+
+    ageLimit: { min: "21", max: "32" },
+    salary: "₹56,100 – ₹2,50,000",
+    eligibility: "Graduate",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 105,
+    title: "UPSC Civil Services",
+    slug: "upsc-civil-services",
+    category: "latest-jobs",
+    location: "All India",
+
+    importantDates: {
+      start: "01 Dec 2025",
+      last: "31 Dec 2025",
+      exam: "April 2026",
+    },
+
+    ageLimit: { min: "21", max: "32" },
+    salary: "₹56,100 – ₹2,50,000",
+    eligibility: "Graduate",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 106,
+    title: "UPSC Civil Services",
+    slug: "upsc-civil-services",
+    category: "latest-jobs",
+    location: "All India",
+
+    importantDates: {
+      start: "01 Dec 2025",
+      last: "31 Dec 2025",
+      exam: "April 2026",
+    },
+
+    ageLimit: { min: "21", max: "32" },
+    salary: "₹56,100 – ₹2,50,000",
+    eligibility: "Graduate",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
   {
     id: 201,
     title: "SSC Admit Card",
+    slug: "ssc-admit-card",
     category: "admit-card",
     location: "India",
 
@@ -65,9 +167,385 @@ export const jobs = [
     eligibility: "-",
 
     links: {
-      apply: "",
-      notification: "",
-      official: "",
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 202,
+    title: "SSC Admit Card",
+    slug: "ssc-admit-card",
+    category: "admit-card",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 203,
+    title: "SSC Admit Card",
+    slug: "ssc-admit-card",
+    category: "admit-card",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+    e: "SSC Admit Card",
+    slug: "ssc-admit-card",
+    category: "admit-card",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 205,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 206,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 207,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 208,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 209,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 210,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 211,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "results",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+
+  {
+    id: 212,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "answer-key",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 213,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "answer-key",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+  {
+    id: 214,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "answer-key",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+
+  {
+    id: 215,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "central-jobs",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
+      admitCard: "https://ssc.nic.in/admitcard",
+    },
+  },
+
+  {
+    id: 216,
+    title: "SSC results",
+    slug: "ssc-results",
+    category: "central-jobs",
+    location: "India",
+
+    importantDates: {
+      start: "-",
+      last: "-",
+      exam: "Download Now",
+    },
+
+    ageLimit: { min: "-", max: "-" },
+    salary: "-",
+    eligibility: "-",
+
+    links: {
+      apply: "https://ssc.gov.in/",
+      notification: "#",
+      official:
+        "https://ssc.gov.in/api/attachment/uploads/masterData/NoticeBoards/notice_01122025.pdf",
       admitCard: "https://ssc.nic.in/admitcard",
     },
   },
